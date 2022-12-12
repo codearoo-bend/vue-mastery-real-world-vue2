b5b9703b9567a9e40bca30487f77cb4b9ce5d079
Blasz <chris.blasz@bendhsa.com>
2022-12-11 22:10:17 -0500

    Added redirect and alias

--- real-world-vue-ui/src/router/index.js
+++ real-world-vue-ui/src/router/index.js
@@ -11,13 +11,20 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
+    path: "/about-us",
     name: "about",
+    alias: "/about-me" /* another option instead of redirect,
+     but keeps the alias URL instead of replacing it. */,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/about",
+    redirect: { name: "about" }, // using the named route to redirect to.
+    // alternatively, could be: redirect: "/about-us"
   },
 ];
 
