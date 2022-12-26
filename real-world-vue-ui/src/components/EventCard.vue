4b29818f3b84827b91ab880ee74bbc22f71ac64b
Blasz <chris.blasz@bendhsa.com>
2022-12-26 14:27:15 -0500

    Moved the axios call to the API into a centralize location. Added details to be shown in the EventShow.vue and calling the API from there.

--- real-world-vue-ui/src/components/EventCard.vue
+++ real-world-vue-ui/src/components/EventCard.vue
@@ -1,8 +1,8 @@
 <template>
-  <router-link :to="{ name: 'event-show', params: { id: '1' } }">
+  <router-link :to="{ name: 'event-show', params: { id: event.id } }">
     <div class="event-card -shadow">
       <span class="eyebrow"> @{{ event.time }} on {{ event.date }} </span>
-      <h4 class="title">{{ event.titlee }}</h4>
+      <h4 class="title">{{ event.title }}</h4>
       <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon>
     </div>
   </router-link>
--- real-world-vue-ui/src/views/EventShow.vue
+++ real-world-vue-ui/src/views/EventShow.vue
@@ -1,9 +1,47 @@
 <template>
-  <h1>Showing Event {{ id }}</h1>
+  <div>
+    <div class="header-event">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h1 class="title">{{ event.title }}</h1>
+      <h5>Organized by {{ event.organizer }}</h5>
+      <h5>Category: {{ event.category }}</h5>
+    </div>
+    <BaseIcon name="map"><h2>Location</h2></BaseIcon>
+    <address>{{ event.location }}</address>
+    <h2>Event Details</h2>
+    <p>{{ event.description }}</p>
+    <h2>
+      Attendies
+      <span class="badge -fill-gradient">{{ event.attendees.length }}</span>
+    </h2>
+    <ul class="list-group">
+      <li v-for="(attendee, index) in event.attendees" :key="index">
+        <b>{{ attendee.name }}</b>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import EventService from "@/services/EventService.js";
+
 export default {
   props: ["id"],
+  data() {
+    return {
+      event: {},
+    };
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((response) => {
+        this.event = response.data;
+        console.log(this.event);
+      })
+      .catch((error) => {
+        console.log("caught error:", error);
+        console.log("caught error response:", error.response);
+      });
+  },
 };
 </script>
